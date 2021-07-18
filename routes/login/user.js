012d6b7c610939369785bba511e18352b8809d9a
Juan Carlos Campbell <jc.campbellg@gmail.com>
2021-07-17 20:14:19 -0600

    Change user API to only return no params

--- routes/login/user.js
+++ routes/login/user.js
@@ -5,14 +5,7 @@ const moment = require('moment');
 const auth = require('../../auth');
 
 router.get('/', auth, (req, res) => {
-  const userId = req.body.userId;
-  User.findOne({'_id': userId}, (err, user) => {
-    if (err) {
-      res.status(400).send({message: 'No hay usuario con ese Id'});
-    } else {
-      res.json(user);
-    }
-  })
+  res.json(req.user);
 });
 
 module.exports = router;
