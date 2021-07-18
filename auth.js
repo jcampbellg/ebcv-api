cbe881933520d40ca36acc9e66b0fb62f260f00a
Juan Carlos Campbell <jc.campbellg@gmail.com>
2021-07-17 20:07:09 -0600

    Remove try catch

--- auth.js
+++ auth.js
@@ -1,24 +1,18 @@
 const User = require('./db/userModel');
 
 module.exports = async (req, res, next) => {
-  try {
-    //   get the token from the authorization header
-    const token = await req.headers.authorization.split(" ")[1];
+  //   get the token from the authorization header
+  const token = await req.headers.authorization.split(" ")[1];
 
-    // Get User by authkey
-    User.findOne({'_id': token}, (err, user) => {
-      if (err) {
-        res.status(401).json({
-          error: new Error('Not Authorize'),
-        });
-      } else {
-        req.user = user;
-        next();
-      }
-    });
-  } catch (error) {
-    res.status(401).json({
-      error: new Error('Not Authorize'),
-    });
-  }
+  // Get User by authkey
+  User.findOne({'_id': token}, (err, user) => {
+    if (err) {
+      res.status(401).json({
+        error: new Error('Not Authorize'),
+      });
+    } else {
+      req.user = user;
+      next();
+    }
+  });
 };
